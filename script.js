561ddcebb037388953b4714e5da95ad9d419b00d
Sebastian_LeonD <122692806+SebLeDz@users.noreply.github.com>
2023-01-23 22:48:47 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function encriptar(stringEncriptada){
     let matrizCodigo = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase()
 
-    for(let i = 0 ; i < matrizCodigo.length, i++;){
+    for(let i = 0 ; i < matrizCodigo.length; i++){
         if(stringEncriptada.includes(matrizCodigo[i][0])){
             stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])  
         }
